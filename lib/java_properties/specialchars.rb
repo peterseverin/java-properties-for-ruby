ac6af3f9399e27ffdf5db029cff71a871d9d9420
peterseverin <petru.severin@gmail.com>
2012-01-28 19:18:25 +0200

    Update lib/java_properties/specialchars.rb

--- lib/java_properties/specialchars.rb
+++ lib/java_properties/specialchars.rb
@@ -12,7 +12,7 @@ module JavaProperties
       
       def self.encode string
 	# Replace special chars with proper \n escaped characters
-	string.gsub!( /([\t\n\r\f])/ ) do |c|
+	string.gsub( /([\t\n\r\f\\])/ ) do |c|
 	  char = $1
 	  case char
 	  when "\t"
@@ -23,16 +23,17 @@ module JavaProperties
 	    '\\r'
 	  when "\f"
 	    '\\f'
+	  when "\\"
+	    '\\\\'
 	  end
 	end
-	string
       end
       
       # Decodes any \X characters read in from a properties file.
       
       def self.decode string
 	# Replace \n escaped chars with proper characters
-	string.gsub!( /\\(.)/ ) do |c|
+	string.gsub( /\\(.)/ ) do |c|
 	  char = $1 if c =~ /\\(.)/
 	  case char
 	  when 't'
@@ -47,7 +48,6 @@ module JavaProperties
 	    char
 	  end
 	end
-	string
       end
     
     end
